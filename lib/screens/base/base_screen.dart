2ef93808c13c0af4e255e9076cdeb4107bd44a12
Marcelo Camillo <marcelocamillo18@gmail.com>
2020-07-25 02:16:16 -0300

    Criando o Drawer Customizado

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/common/custom_drawer/custom_drawer.dart';
 
 class BaseScreen extends StatelessWidget {
   final PageController pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-// PageView: para alterar entre uma tela e outra de forma simples
     return PageView(
         controller: pageController,
-        // impede que a PageView seja movimentada através de gestos
         physics: const NeverScrollableScrollPhysics(),
         children: <Widget>[
+          Scaffold(
+            drawer: CustomDrawer(),
+            appBar: AppBar(
+              title: const Text('Home')
+            )
+          ),
           Container(color: Colors.red),
           Container(color: Colors.yellow),
           Container(color: Colors.green)
