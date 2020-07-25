80c28383865588a8bdb0afc21eef9e2a783489a7
Marcelo Camillo <marcelocamillo18@gmail.com>
2020-07-25 20:04:10 -0300

    Navegando via Provider

--- lib/common/custom_drawer/drawer_tile.dart
+++ lib/common/custom_drawer/drawer_tile.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/page_manager.dart';
+import 'package:provider/provider.dart';
 
 class DrawerTile extends StatelessWidget {
   const DrawerTile({this.iconData, this.title, this.page});
@@ -9,9 +11,11 @@ class DrawerTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final int curPage = context.watch<PageManager>().page;
+
     return InkWell(
       onTap: () {
-        debugPrint('toquei $page');
+        context.read<PageManager>().setPage(page);
       },
       child: SizedBox(
         height: 60,
@@ -22,19 +26,19 @@ class DrawerTile extends StatelessWidget {
               child: Icon(
                 iconData,
                 size: 32,
-                color: Colors.grey[700]
-              ),
+                color: curPage == page ? Colors.red : Colors.grey[700]
+              )
             ),
             Text(
               title,
               style: TextStyle(
                 fontSize: 16,
-                color: Colors.grey[700]
+                  color: curPage == page ? Colors.red : Colors.grey[700]
               )
             )
           ]
-        ),
-      ),
+        )
+      )
     );
   }
 }
--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,25 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/common/custom_drawer/custom_drawer.dart';
+import 'package:loja_virtual/models/page_manager.dart';
+import 'package:provider/provider.dart';
 
 class BaseScreen extends StatelessWidget {
   final PageController pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-        controller: pageController,
-        physics: const NeverScrollableScrollPhysics(),
-        children: <Widget>[
-          Scaffold(
-            drawer: CustomDrawer(),
-            appBar: AppBar(
-              title: const Text('Home')
-            )
-          ),
-          Container(color: Colors.red),
-          Container(color: Colors.yellow),
-          Container(color: Colors.green)
-        ]
+    return Provider(
+      create: (_) => PageManager(pageController),
+      child: PageView(
+          controller: pageController,
+          physics: const NeverScrollableScrollPhysics(),
+          children: <Widget>[
+            Scaffold(
+              drawer: CustomDrawer(),
+              appBar: AppBar(
+                title: const Text('Home')
+              )
+            ),
+            Scaffold(
+                drawer: CustomDrawer(),
+                appBar: AppBar(
+                    title: const Text('Produtos')
+                )
+            ),
+            Scaffold(
+                drawer: CustomDrawer(),
+                appBar: AppBar(
+                    title: const Text('Meus Pedidos')
+                )
+            ),
+            Scaffold(
+                drawer: CustomDrawer(),
+                appBar: AppBar(
+                    title: const Text('Lojas')
+                )
+            ),
+          ]
+      ),
     );
   }
 }
