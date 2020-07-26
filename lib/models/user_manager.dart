d12b6363429ebf189bb163e923eb7a9f97d7c0a5
Marcelo Camillo <marcelocamillo18@gmail.com>
2020-07-25 21:46:26 -0300

    Bloqueando os Campos Durante o Login

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -1,21 +1,35 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:loja_virtual/helpers/firebase_errors.dart';
 import 'package:loja_virtual/models/user.dart';
 
-class UserManager {
+class UserManager extends ChangeNotifier {
   final FirebaseAuth auth = FirebaseAuth.instance;
 
+  bool loading = false;
+
   Future<void> signIn({User user, Function onFail, Function onSuccess}) async {
+    setLoading(true);
+
     try {
       final AuthResult result = await auth.signInWithEmailAndPassword(
         email: user.email,
         password: user.password
       );
 
+      await Future.delayed(Duration(seconds: 2));
+
       onSuccess();
     } on PlatformException catch(e) {
       onFail(getErrorString(e.code));
     }
+
+    setLoading(false);
+  }
+
+  void setLoading(bool value) {
+    loading = value;
+    notifyListeners();
   }
 }
