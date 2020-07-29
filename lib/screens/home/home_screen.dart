04af9ca41eb36d1211d7988eac1dddb5768a2ad1
Marcelo Camillo <marcelocamillo18@gmail.com>
2020-07-28 22:48:40 -0300

    Criando a Lista de Seções

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/common/custom_drawer/custom_drawer.dart';
+import 'package:loja_virtual/models/home_manager.dart';
+import 'package:provider/provider.dart';
+
+import 'components/section_list.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -39,11 +43,25 @@ class HomeScreen extends StatelessWidget {
                   )
                 ]
               ),
-              SliverToBoxAdapter(
-                child: Container(
-                  height: 2000,
-                  width: 200,
-                )
+              Consumer<HomeManager>(
+                builder: (_, homeManager, __) {
+                  final List<Widget> children = homeManager.sections.map<Widget>(
+                    (section) {
+                      switch(section.type) {
+                        case 'List':
+                          return SectionList(section);
+                        case 'Staggered':
+                          return Container();
+                        default:
+                          return Container();
+                      }
+                    }
+                  ).toList();
+
+                  return SliverList(
+                    delegate: SliverChildListDelegate(children),
+                  );
+                }
               )
             ]
           )
