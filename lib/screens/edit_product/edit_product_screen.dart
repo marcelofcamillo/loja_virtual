a59fd4e4f056adba42d2c0cb93c99c32f63bc45a
Marcelo Camillo <marcelocamillo18@gmail.com>
2020-07-30 19:46:23 -0300

    Formulário Customizado para Imagens

--- lib/screens/edit_product/edit_product_screen.dart
+++ lib/screens/edit_product/edit_product_screen.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/models/product.dart';
+
+import 'components/images_form.dart';
 
 class EditProductScreen extends StatelessWidget {
+  const EditProductScreen(this.product);
+
+  final Product product;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Editar anúncio'),
         centerTitle: true
+      ),
+      body: ListView(
+        children: <Widget>[
+          ImagesForm(product)
+        ]
       )
     );
   }
