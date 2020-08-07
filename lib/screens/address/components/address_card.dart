d6238d247732a4e1a5da4cb050e7b75d6d6fe1ad
Marcelo Camillo <marcelocamillo18@gmail.com>
2020-08-06 21:20:47 -0300

    Criando a Tela de Endereço

---
+++ lib/screens/address/components/address_card.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'cep_input_field.dart';
+
+class AddressCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 16, 16, 4),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Endereço de entrega',
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600
+              )
+            ),
+            CepInputField()
+          ]
+        )
+      )
+    );
+  }
+}
